c587affb994b6c7b74d6b9f30d542c280881643a
= <=>
2019-01-22 15:52:58 +0700

    first commit

---
+++ webpaypal/application/views/admin/footer.php
@@ -0,0 +1,9 @@
+<div class="clear"></div>
+<div class="clear mt30"></div>
+<div id="footer">
+		<div class="wrapper">
+		
+		
+		</div>
+		
+</div>
---
+++ webpaypal/application/views/site/footer.php
@@ -0,0 +1,15 @@
+
+			    <!-- The box-footer-->
+		        
+     <div id="footer_text"> <!-- The footer_text --> 
+	     <p style="text-align:center;"> đồ án web mua bán điện thoại</p>
+	  
+		 
+		 
+	 </div><!-- End footer_text -->
+	 
+	  <div id="footer_face">  <!-- The footer_face -->            
+	        
+      </div><!-- End footer_face -->
+     <div class="clear"></div><!-- clear float --> 		        <!-- End box-footer -->
+		  
